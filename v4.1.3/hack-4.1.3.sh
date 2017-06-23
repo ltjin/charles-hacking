9d6c6f97068c8d6b874a565aeb54fea0030e3b02
jinlongtao <jinlongtao@voici.cn>
2017-06-23 10:59:08 +0800

    for v4.1.3

---
+++ v4.1.3/hack-4.1.3.sh
@@ -0,0 +1,15 @@
+charles=/Applications/Charles.app/Contents/Java/charles.jar
+dir=charleshack
+
+mkdir $dir
+cd $dir
+cat >> psPJ.java <<EOF
+package com.xk72.charles;
+public final class psPJ {
+	public static boolean qIvM() { return true; }
+	public static String tCiz() { return "http://www.gfzj.us"; }
+	public static String qIvM(String name, String key) { return null; }
+}
+EOF
+javac -encoding UTF-8 psPJ.java -d .&& jar -uvf $charles com/xk72/charles/psPJ.class
+cd .. && rm -rf $dir
